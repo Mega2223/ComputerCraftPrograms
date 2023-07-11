b0c44cfbb17708821a94ba5e01cc9cb98999b224
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2023-07-10 21:09:50 -0300

    updateSoft function

---
+++ MegAPI.lua
@@ -0,0 +1,13 @@
+
+function updateSoftware()
+	local API = "https://raw.githubusercontent.com/Mega2223/ComputerCraftPrograms/main/MegAPI.lua"
+	local APIT = "https://raw.githubusercontent.com/Mega2223/ComputerCraftPrograms/main/MegAPITurtle.lua"
+	
+	shell.run("cd /")
+	shell.run("delete MAPI")
+	shell.run("mkdir MAPI")
+	shell.run("cd MAPI")
+	shell.run("wget " .. API .. " MegAPI.lua")
+	shell.run("wget " .. APIT .. " MegAPITurtle.lua")
+	
+end
