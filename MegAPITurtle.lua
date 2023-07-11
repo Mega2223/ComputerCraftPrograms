ff464b7675a34cf1a687dd02cc83721df66661e0
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2023-07-11 01:52:49 -0300

    bugfix

--- MegAPITurtle.lua
+++ MegAPITurtle.lua
@@ -132,14 +132,15 @@ function walkTo (toX, toZ)
 end
 
 function pushForward()
-	--while not moveForward() do
+	while not moveForward() do
+		moveUp()
 		--while not moveUp() do
 		--	acacac = getDirection()
 		--	turnToDirection(inverseDirection(acacac))
 		--	pushForward()
 		--	turnToDirection(acacac)
 		--end
-	--end
+	end
 	while not turtle.detectDown() do
 		moveDown()
 	end
