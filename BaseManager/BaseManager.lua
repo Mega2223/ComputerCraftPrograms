a187aeae60dc43d6534998fa5766e50680721f90
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2023-07-15 20:09:02 -0300

    start of setting up button

--- BaseManager/BaseManager.lua
+++ BaseManager/BaseManager.lua
@@ -2,6 +2,8 @@ require("/.MAPI.MegAPI")
 
 mon = peripheral.wrap("left")
 x,y = mon.getSize()
+mon.setBackgroundColor(colors.black)
+mon.clear()
 --o espaço horizontal deve ser 3/5, mas tira 2, 1 pra distância e outro pro quadrado
 --coordenada do meio só precisa de 1 de diferença
 current = window.create(mon,2,2,math.floor(x*(3.0/5.0))-2,y-2)
@@ -10,7 +12,16 @@ selector = window.create(mon,math.floor(x*(3.0/5.0))+2,2,x-2,y-2)
 function updateSelector()
 	term.redirect(mon)
 	mon.clear()
-	mon.setBackgroundColor(color.white)
-	paintutils.drawBox(3,3,math.floor(x*(3.0/5.0))-1,y-2)
-	paintutils.drawBox(math.floor(x*(3.0/5.0)),3,x-2,y-2)
+	mon.setBackgroundColor(colors.white)
+	paintutils.drawBox(2,2,math.floor(x*(3.0/5.0))-1,y-1)
+	paintutils.drawBox(math.floor(x*(3.0/5.0))+1,2,x-1,y-1)
 end
+
+function waitForButton()
+	local event, button, x, y = os.pullEvent("mouse_click")
+	term.redirect(selector)
+	print(x..":"..y)
+	term.redirect(term.native())
+end
+
+term.redirect(term.native())
