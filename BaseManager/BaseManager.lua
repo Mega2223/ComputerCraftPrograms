9daef6e374a3e50887ab5b60dda58e02d31a8587
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2023-07-15 20:47:05 -0300

    attempt to correct lua not supporting threads

--- BaseManager/BaseManager.lua
+++ BaseManager/BaseManager.lua
@@ -6,8 +6,8 @@ mon.setBackgroundColor(colors.black)
 mon.clear()
 --o espaço horizontal deve ser 3/5, mas tira 2, 1 pra distância e outro pro quadrado
 --coordenada do meio só precisa de 1 de diferença
-current = window.create(mon,2,2,math.floor(x*(3.0/5.0))-2,y-2)
-selector = window.create(mon,math.floor(x*(3.0/5.0))+2,2,x-2,y-2)
+current = window.create(mon,3,2,math.floor(x*(3.0/5.0))-2,y-3)
+selector = window.create(mon,math.floor(x*(3.0/5.0))+2,3,x-2,y-3)
 
 function updateSelector()
 	term.redirect(mon)
@@ -17,11 +17,26 @@ function updateSelector()
 	paintutils.drawBox(math.floor(x*(3.0/5.0))+1,2,x-1,y-1)
 end
 
-function waitForButton()
-	local event, button, x, y = os.pullEvent("mouse_click")
+term.redirect(term.native())
+
+function onMonitorTouch(ev,side,x,y)
 	term.redirect(selector)
 	print(x..":"..y)
 	term.redirect(term.native())
 end
 
-term.redirect(term.native())
+function updateStats()
+	term.redirect(selector)
+	print("stats")
+	term.redirect(term.native())
+end
+
+while true do 
+	os.startTimer(0.1)
+	local event, a1, a2, a3 = os.pullEvent()
+	if event == 'monitor_touch' then
+		onMonitorTouch(event,a1,a2,a3)
+	else
+		updateStats()
+	end
+end
